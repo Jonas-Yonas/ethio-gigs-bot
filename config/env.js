ac7b773940a70d04a49591d902b396f9692e8f3a
@yonas_gashaw <Yonas1520@gmail.com>
2025-04-20 20:25:48 +0300

    Nullish check added

--- config/env.js
+++ config/env.js
@@ -3,4 +3,4 @@ dotenv.config();
 
 export const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 export const MONGODB_URI = process.env.MONGODB_URI;
-export const ADMIN_IDS = process.env.ADMIN_IDS.split(",");
+export const ADMIN_IDS = process.env.ADMIN_IDS?.split(",");
